6b501e71a30ad393a69c10f3d18464290103787f
Bagus Surya Utama <e959201@gmail.com>
2018-10-31 15:40:45 +0700

    template AdminLTE 2.4.5

---
+++ resources/views/coba1.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('content-header')
+<h1>
+    Coba1
+    <small>Control Panel</small> 
+</h1>
+<ol class="breadcrumb">
+    <li><a href="/"> <i class="fa fa-dashboard"></i>Home</a></li>
+    <li class="active">Coba1</li>
+</ol>
+@endsection
+
+@section('content')
+    Ini coba1
+@endsection
